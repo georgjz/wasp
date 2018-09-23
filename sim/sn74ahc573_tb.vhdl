1f724df0ca16c45e47c335124fca8918e210f148
Georg Ziegler <georgjziegler@gmail.com>
2018-09-23 04:28:59 +0200

    Update to concurrent statements

--- sim/sn74ahc573_tb.vhdl
+++ sim/sn74ahc573_tb.vhdl
@@ -1,4 +1,4 @@
--- Test Bench for SN74AHC573
+-- Test Bench for SN74AHC573 model
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -8,10 +8,10 @@ end entity sn74ahc573_tb;
 
 -- test bench architecture
 architecture test_bench of sn74ahc573_tb is
-    signal oe_n, le, d, q : std_logic := '1';
+    signal oe_n, le, d, q : std_logic := '0';
 begin
 
-    dut : entity work.sn74ahc573(behave)
+    dut : entity work.sn74ahc573(rtl)
         port map ( oe_n, le, d, q );
 
     stimulus : process is
@@ -20,12 +20,24 @@ begin
         oe_n <= '1'; wait for 20 ns;
 
         -- load some latches
-        oe_n <= '0'; le <= '1'; d <= '0'; wait for 20 ns;
-        oe_n <= '0'; le <= '1'; d <= '1'; wait for 20 ns;
-        oe_n <= '0'; le <= '0'; d <= '1'; wait for 20 ns;
-        oe_n <= '0'; le <= '0'; d <= '0'; wait for 20 ns;
-        oe_n <= '1'; le <= '1'; d <= '0'; wait for 20 ns;
-        oe_n <= '1'; le <= '1'; d <= '1'; wait for 20 ns;
+        -- oe_n <= '0'; le <= '1'; d <= '0'; wait for 100 ns;
+        -- oe_n <= '0'; le <= '1'; d <= '1'; wait for 100 ns;
+        -- oe_n <= '0'; le <= '0'; d <= '1'; wait for 100 ns;
+        -- oe_n <= '0'; le <= '0'; d <= '0'; wait for 100 ns;
+        -- oe_n <= '1'; le <= '1'; d <= '0'; wait for 100 ns;
+        -- oe_n <= '1'; le <= '1'; d <= '1'; wait for 100 ns;
+
+        -- WARNING: These signal changes violate metastability
+        oe_n <= '0'; le <= '1'; d <= '1'; wait for 4 ns;
+            -- violate minimal pulse width
+        -- le <= '0'; wait for 1 ns;
+            -- violate setup time
+        -- d <= '0';  wait for 1 ns;
+        -- le <= '0'; wait for 1 ns;
+            -- violate hold time
+        wait for 1 ns; le <= '0';
+        wait for 1 ns; d <= '0';
+        wait for 1 ns;
 
         -- wait forever
         wait;
--- src/sn74ahc573.vhdl
+++ src/sn74ahc573.vhdl
@@ -1,4 +1,4 @@
--- behavioral model of a SN74AHC573 D-type Transparent Latch
+-- simple model of a SN74AHC573 D-type Transparent Latch
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -8,28 +8,49 @@ entity sn74ahc573 is
            d : in std_logic;            -- data input
            q : out std_logic );         -- data output
 
-    -- TODO: add timing constants
+    -- TODO: add detailed timing constants
+    constant t_pd   : delay_length := 9 ns;     -- Propagation delay
+    constant t_w    : delay_length := 5 ns;     -- Pulse duration, LE high
+    constant t_su   : delay_length := 3.5 ns;   -- Setup time, data before LE falling edge
+    constant t_h    : delay_length := 1.5 ns;   -- Hold time, data after LE falling edge
 end entity sn74ahc573;
 
--- behavioral architecture to chech metastability
-architecture behave of sn74ahc573 is
+-- rtl architecture to check metastability
+architecture rtl of sn74ahc573 is
     signal intern : std_logic := 'X';
 begin
 
-    behavioral : process (oe_n, le, d) is
-    begin
-        -- check for high impedance
-        if oe_n = '1' then
-            intern <= 'Z';
-        else
-            if le = '1' then
-                intern <= d;
-            -- else
-                -- intern <= unaffected;
-            end if;
-        end if;
-    end process behavioral;
+    -- concurrent replacement of behavioral process
+    -- TODO: replace with state machine for more precise propagation delays
+    intern <= 'Z' when oe_n = '1' else              -- high-impedance state
+               d  when oe_n = '0' and le = '1' else -- latch input
+               unaffected;                          -- else, nothing happens
 
+    -- check metastability of latch enable signal
+    checkMetaStability : process is
+    begin
+        -- wait for LE falling edge
+        wait until falling_edge(le);
+
+        --check pulse width
+        assert le'delayed'stable(t_w)
+            report "LE pulse width too short!"
+            severity failure;
+
+        -- check setup time
+        assert intern'delayed'stable(t_su)
+            report "Input changed during setup time!"
+            severity failure;
+
+        -- check hold time
+        wait for t_h;
+        assert intern'delayed'stable(t_h + t_su)
+            report "Input signal changed during hold time!"
+            severity failure;
+
+    end process checkMetaStability;
+
+    -- update output
     q <= intern;
 
-end architecture behave;
+end architecture rtl;
