ac4d8b75a15c156103b2dff2be462d3e10629805
Georg Ziegler <georgjziegler@gmail.com>
2018-09-29 02:22:36 +0200

    Add files for RAM chip

---
+++ sim/idt6116xaxx_tb.vhdl
@@ -0,0 +1,30 @@
+-------------------------------------------------------------------------------
+--
+-- unit name: Test bench for IDT6116xAxx (idt6116xaxx_tb)
+-- author: Georg Ziegler
+--
+-- description: This test bench verfies functionality of the idt6116xaxx unit
+--
+-- dependencies: ieee library
+--
+-------------------------------------------------------------------------------
+-- TODO:
+--
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- entity declaration
+entity idt6116xaxx_tb is
+end entity idt6116xaxx_tb;
+
+-- test bench architecture
+architecture test_bench of idt6116xaxx_tb is
+    -- signal
+begin
+
+    -- code
+
+end architecture test_bench;
